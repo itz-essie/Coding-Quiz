76fe229cc388e0ecbaa3ade5f7e6086c6e9a2bdf
Esiena Ekwofia <eekwofia@gmail.com>
2020-12-02 08:28:54 -0500

    adding a function to count score, as well as adding a function to check user answer against correct answer.

--- javaquiz.js
+++ javaquiz.js
@@ -1,120 +1,109 @@
 // Create the necessary elements to use later
 var body = document.body;
-const startBtn = document.getElementById("startBtn");
-const highscoresBtn = document.getElementById("highscoresBtn");
+var startBtn = document.getElementById("startBtn");
+var highscoresBtn = document.getElementById("highscoresBtn");
 
-const questionsDiv = document.getElementById("questions");
-const choicesDiv = document.getElementById("choices");
-const timerDiv = document.getElementById("timer");
-const counterDiv = document.getElementById("counter");
-let currentQuestion = 0;
+var questionsDiv = document.getElementById("questions");
+var choicesDiv = document.getElementById("choices");
+var timerDiv = document.getElementById("timer");
+var counterDiv = document.getElementById("counter");
+var currentQuestion = 0;
+var score= 0;
+var instructionsDiv = document.getElementById("instructions");
 
-// When you click button to begin quiz, timer counts down
-startBtn.onClick = () => {
-  document.getElementsByClassName(".container").style.display = "none";
-  // quizDir.style.display = "none"
-};
 // Start button and timer begins
 document.getElementById("startBtn").addEventListener("click", function () {
+  instructionsDiv.textContent = "";
+  startBtn.style.display = "none"
   var timeleft = 60;
 
   var downloadTimer = setInterval(function seconds() {
     document.getElementById("count").innerHTML =
       timeleft + "&nbsp" + "seconds remaining";
 
+// Timer reaches 0 and Time is up, quiz over. 
     timeleft -= 1;
     if (timeleft <= 0) {
       clearInterval(downloadTimer);
       document.getElementById("count").innerHTML = "Time is up!";
     }
   }, 1000);
-  var h3Tag = document.createElement("h3");
-  h3Tag.textContent = questions[currentQuestion].q;
-  document.querySelector(".container").appendChild(h3Tag);
+
+  //Display questions in the container 
+  var h2Tag = document.createElement("h2");
+  h2Tag.textContent = questions[currentQuestion].q;
+  document.querySelector(".container").appendChild(h2Tag);
 
   for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
     var questionButtons = document.createElement("button");
     questionButtons.setAttribute("class", "btn");
     questionButtons.textContent = questions[currentQuestion].answers[i];
     document.querySelector(".container").appendChild(questionButtons);
-    
-}
-
   }
-  
-);
-
-
-
-/*  Add listener for the next question button
-$('#next-question-button').click(function() {
-  if (current_question_index < self.questions.length - 1) {
-    current_question_index++;
-    change_question();
-  } 
-  */
-
-  /* on submit, show results 
-  submitButton.onclick = function(){ showResults(questions, quizContainer, resultsContainer); }
-*/
+});
 
 // create questions here
 let questions = [
   {
     q: "Hyper Text Markup Language Stand For?",
-    answers: [
-    'JavaScript', 
-    'XHTML', 
-    'CSS',
-    'HTML'],
-    correctAnswer: 'D'
+    choices: ["JavaScript", "XHTML", "CSS", "HTML"],
+    correctAnswer: "HTML",
   },
   {
     q: "Which language is used for styling web pages?",
-    answers: [
-    'HTML', 
-    'JQuery', 
-    'CSS', 
-    'XML'],
-    correctAnswer: 'C'
+    choices: ["HTML", "JQuery", "CSS", "XML"],
+    correctAnswer: "CSS",
   },
   {
-    q: "What tag can be used to insert a line break or blank line in an HTML document?",
-    answers: [
-    '<title>',
-    '<head>',
-    '<body>',
-    '<br>'],
-    correctAnswer: 'D'
+    q:
+      "What tag can be used to insert a line break or blank line in an HTML document?",
+    choices: ["<title>", "<head>", "<body>", "<br>"],
+    correctAnswer: "<br>",
   },
   {
-    q: "What declaration MUST be included as the first item in an HTML document before the tag and is used to provide instructions to the web browser?",
-    answers: [
-    '<!DOCTYPE>',
-    '<code>',
-    '<embed>',
-    '<caption>'],
-    correctAnswer: 'A'
+    q:
+      "What declaration MUST be included as the first item in an HTML document before the tag and is used to provide instructions to the web browser?",
+    choices: ["<!DOCTYPE>", "<code>", "<embed>", "<caption>"],
+    correctAnswer: "<!DOCTYPE>",
   },
   {
-    q: "What is a JavaScript element that represents either TRUE or FALSE values?",
-    answers: [
-    'Condition',
-    'Boolean',
-    'Funtion',
-    'String'],
-    correctAnswer: 'B'
+    q:
+      "What is a JavaScript element that represents either TRUE or FALSE values?",
+    choices: ["Condition", "Boolean", "Funtion", "String"],
+    correctAnswer: "Boolean",
   },
   {
-    q: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
-    answers: [
-    'Conditional Loop',
-    'Else Loop',
-    'While Loop',
-    'For Loop'],
-    correctAnswer: 'C'
+    q:
+      "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+    choices: ["Conditional Loop", "Else Loop", "While Loop", "For Loop"],
+    correctAnswer: "While Loop",
   },
 ];
+
+
+
+// This function will check the correct answer against the user choice
+// It will also load the next question, or if it is the last question, will show gameOver()
+function checkAnswer(event) {
+  event.preventDefault();
+  var wrongAnswer = 10;
+  var q = questionList[currentQuestion];
+  var userInput = this.children[0].getAttribute("data-answer");
+  if (userInput === q.correctAnswer) {
+    score++;
+    alert("Correct!");
+  } else {
+    countDown = countDown - wrongAnswer;
+    countDown.textContent = countDown;
+    alert("Wrong Answer!");
+  }
+  if (currentQuestionIndex < lastQuestionIndex) {
+    currentQuestionIndex++;
+    renderQuestion();
+  } else {
+    alert("Game Over! Your total score is: " + score + "!");
+  }
+}
 
 /* GIVEN I am taking a code quiz
 WHEN I click the start button
